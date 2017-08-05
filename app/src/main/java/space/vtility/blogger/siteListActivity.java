290383ef942e0e0fc5fc80323062b311da6a6784
geekercai <cairunpeimail@gmail.com>
2017-08-05 09:45:15 +0800

    add some fc

--- app/src/main/java/space/vtility/blogger/siteListActivity.java
+++ app/src/main/java/space/vtility/blogger/siteListActivity.java
@@ -2,25 +2,26 @@ package space.vtility.blogger;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-
-import com.google.gson.Gson;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.List;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+
+
 public class siteListActivity extends AppCompatActivity {
-    //String siteDomain = SiteView.mSiteDomain;
+    String[] postTitle = new String[200];
+    String[] path = new String[200];
+    String siteDomain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,49 +30,59 @@ public class siteListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        SharedPreferences pref = getSharedPreferences("swap",MODE_PRIVATE);
-        String siteDomain = pref.getString("swap","https://www.imys.net/");//github 插件作者的blog
+        SharedPreferences pref = getSharedPreferences("swap", MODE_PRIVATE);
+        siteDomain = pref.getString("swap", "https://www.imys.net/");//插件作者的blog
 
-        StringBuffer domain = new StringBuffer(siteDomain);
-        domain.append("/api/posts.json");
-        String post = domain.toString();
+        sendRequestWithOkHttp();
 
-        //get posts list
-        try{
-            OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(post)
-                .build();
-        Response response = client.newCall(request).execute();
-        String responseData = response.body().string();
-            parseJSONWithJSONObject(responseData);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                siteListActivity.this, android.R.layout.simple_list_item_1, postTitle);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
 
     }
-    private void parseJSONWithJSONObject(String jsonData){
 
-        try{
+    private void sendRequestWithOkHttp() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    StringBuffer domain = new StringBuffer(siteDomain);
+                    domain.append("/api/posts.json");
+                    final String post = domain.toString();
+                    OkHttpClient client = new OkHttpClient();
+
+                    Request request = new Request.Builder()
+                            .url(post)
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    Log.d("siteListActivity", "output" + responseData);
+                    parseJSONWithJSONObject(responseData);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+    }
+
+    private void parseJSONWithJSONObject(String jsonData) {
+
+
+        try {
             JSONArray jsonArray = new JSONArray(jsonData);
-            for (int i = 0; i < jsonArray.length(); i++){
+            for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
-                String title = jsonObject.getString("title");
-                String path = jsonObject.getString("path");
-        }
+                postTitle[i] = jsonObject.getString("title");
+                path[i] = jsonObject.getString("path");
+            }
 
-    }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-         }
+        }
     }
 }
+
